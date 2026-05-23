18484667fd808e16abf330b495d0ac2477622b80
Vladislav Shchapov <vladislav@shchapov.ru>
2026-05-23 22:39:17 +0500

    eal/loongarch: improve vector header for LoongArch LSX/LASX
    Add the definition of rte_xmm and xmm_t for LSX implementation.
    Add the definition of rte_ymm and ymm_t for LASX implementation.
    
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- lib/eal/loongarch/include/rte_vect.h
+++ lib/eal/loongarch/include/rte_vect.h
@@ -1,62 +1,68 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright(c) 2022 Loongson Technology Corporation Limited
+ * Copyright(c) 2026 Vladislav Shchapov <vladislav@shchapov.ru>
  */
 
 #ifndef RTE_VECT_LOONGARCH_H
 #define RTE_VECT_LOONGARCH_H
 
+/**
+ * @file
+ *
+ * RTE LSX/LASX related header.
+ */
+
+#include <assert.h>
 #include <stdint.h>
+#include <rte_config.h>
+#include <rte_common.h>
 #include "generic/rte_vect.h"
-#include "rte_common.h"
+
+#include <lsxintrin.h>
+#ifdef __loongarch_asx
+#include <lasxintrin.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define RTE_VECT_DEFAULT_SIMD_BITWIDTH RTE_VECT_SIMD_DISABLED
+#define RTE_VECT_DEFAULT_SIMD_BITWIDTH RTE_VECT_SIMD_128
 
-typedef union __rte_aligned(16) xmm {
-	int8_t   i8[16];
-	int16_t  i16[8];
-	int32_t  i32[4];
-	int64_t  i64[2];
-	uint8_t  u8[16];
-	uint16_t u16[8];
-	uint32_t u32[4];
-	uint64_t u64[2];
-	double   pd[2];
-} xmm_t;
+typedef __m128i xmm_t;
 
-#define XMM_SIZE        (sizeof(xmm_t))
-#define XMM_MASK        (XMM_SIZE - 1)
+#define	XMM_SIZE	16
+#define	XMM_MASK	(XMM_SIZE - 1)
 
-typedef union __rte_aligned(16) rte_xmm {
-	xmm_t	 x;
-	uint8_t	 u8[XMM_SIZE / sizeof(uint8_t)];
+static_assert(sizeof(xmm_t) == XMM_SIZE, "");
+
+typedef union rte_xmm {
+	xmm_t    x;
+	uint8_t  u8[XMM_SIZE / sizeof(uint8_t)];
 	uint16_t u16[XMM_SIZE / sizeof(uint16_t)];
 	uint32_t u32[XMM_SIZE / sizeof(uint32_t)];
 	uint64_t u64[XMM_SIZE / sizeof(uint64_t)];
 	double   pd[XMM_SIZE / sizeof(double)];
 } rte_xmm_t;
 
-static inline xmm_t
-vect_load_128(void *p)
-{
-	xmm_t ret = *((xmm_t *)p);
+#ifdef __loongarch_asx
 
-	return ret;
-}
+typedef __m256i ymm_t;
 
-static inline xmm_t
-vect_and(xmm_t data, xmm_t mask)
-{
-	rte_xmm_t ret = {.x = data };
-	rte_xmm_t m = {.x = mask };
-	ret.u64[0] &= m.u64[0];
-	ret.u64[1] &= m.u64[1];
+#define	YMM_SIZE	(sizeof(ymm_t))
+#define	YMM_MASK	(YMM_SIZE - 1)
 
-	return ret.x;
-}
+typedef union rte_ymm {
+	ymm_t    y;
+	xmm_t    x[YMM_SIZE / sizeof(xmm_t)];
+	uint8_t  u8[YMM_SIZE / sizeof(uint8_t)];
+	uint16_t u16[YMM_SIZE / sizeof(uint16_t)];
+	uint32_t u32[YMM_SIZE / sizeof(uint32_t)];
+	uint64_t u64[YMM_SIZE / sizeof(uint64_t)];
+	double   pd[YMM_SIZE / sizeof(double)];
+} rte_ymm_t;
+
+#endif /* __loongarch_asx */
 
 #ifdef __cplusplus
 }
