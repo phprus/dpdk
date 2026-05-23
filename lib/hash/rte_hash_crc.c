37f17354f11af630c45dcd6789dacc0e7d3aca1f
Vladislav Shchapov <vladislav@shchapov.ru>
2026-05-23 23:21:23 +0500

    hash: use LoongArch CRC32C instructions
    LoongArch has CRC32 instructions (polynomials: IEEE802.3 and Castagnoli)
    in basic architecture.
    
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- lib/hash/rte_hash_crc.c
+++ lib/hash/rte_hash_crc.c
@@ -26,6 +26,7 @@ uint8_t rte_hash_crc32_alg = CRC32_SW;
  *   - (CRC32_SSE42) Use SSE4.2 intrinsics if available
  *   - (CRC32_SSE42_x64) Use 64-bit SSE4.2 intrinsic if available (default x86)
  *   - (CRC32_ARM64) Use ARMv8 CRC intrinsic if available (default ARMv8)
+ *   - (CRC32_LOONGARCH64) Use LoongArch CRC intrinsic if available (default LoongArch)
  */
 RTE_EXPORT_SYMBOL(rte_hash_crc_set_alg)
 void
@@ -54,6 +55,14 @@ rte_hash_crc_set_alg(uint8_t alg)
 		rte_hash_crc32_alg = CRC32_ARM64;
 #endif
 
+#if defined RTE_ARCH_LOONGARCH
+	if (!(alg & CRC32_LOONGARCH64))
+		HASH_CRC_LOG(WARNING,
+			"Unsupported CRC32 algorithm requested using CRC32_LOONGARCH64");
+	if (rte_cpu_get_flag_enabled(RTE_CPUFLAG_CRC32))
+		rte_hash_crc32_alg = CRC32_LOONGARCH64;
+#endif
+
 	if (rte_hash_crc32_alg == CRC32_SW)
 		HASH_CRC_LOG(WARNING,
 			"Unsupported CRC32 algorithm requested using CRC32_SW");
@@ -66,6 +75,8 @@ RTE_INIT(rte_hash_crc_init_alg)
 	rte_hash_crc_set_alg(CRC32_SSE42_x64);
 #elif defined(RTE_ARCH_ARM64) && defined(__ARM_FEATURE_CRC32)
 	rte_hash_crc_set_alg(CRC32_ARM64);
+#elif defined(RTE_ARCH_LOONGARCH)
+	rte_hash_crc_set_alg(CRC32_LOONGARCH64);
 #else
 	rte_hash_crc_set_alg(CRC32_SW);
 #endif
