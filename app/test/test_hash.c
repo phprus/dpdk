403121a8b303feff6b0543a37414317bf8ad0596
Vladislav Shchapov <vladislav@shchapov.ru>
2026-05-23 23:21:23 +0500

    hash: use LoongArch CRC32C instructions
    LoongArch has CRC32 instructions (polynomials: IEEE802.3 and Castagnoli)
    in basic architecture.
    
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- app/test/test_hash.c
+++ app/test/test_hash.c
@@ -231,6 +231,13 @@ test_crc32_hash_alg_equiv(void)
 			printf("Failed checking CRC32_SW against CRC32_ARM64\n");
 			break;
 		}
+
+		/* Check against 8-byte-operand LoongArch CRC32 if available */
+		rte_hash_crc_set_alg(CRC32_LOONGARCH64);
+		if (hash_val != rte_hash_crc(data64, data_len, init_val)) {
+			printf("Failed checking CRC32_SW against CRC32_LOONGARCH64\n");
+			break;
+		}
 	}
 
 	/* Resetting to best available algorithm */
