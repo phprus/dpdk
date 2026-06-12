c2d5a13a8e3cf3919b9720ce9c5f697471acee07
Vladislav Shchapov <vladislav@shchapov.ru>
2026-06-13 02:12:32 +0500

    eal/loongarch: add of LASX 128-bit lane helper intrinsics for clang
    Add of LASX 128-bit lane helper intrinsics implementation for clang.
    This patch makes use of the implementation provided by llama.cpp to make it
    possible to compile code on LoongArch64 with clang compiler.
    
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- lib/eal/loongarch/include/rte_vect.h
+++ lib/eal/loongarch/include/rte_vect.h
@@ -138,6 +138,12 @@ typedef union rte_ymm {
 
 #ifndef __loongarch_asx_sx_conv
 
+#ifdef __clang__
+
+#include "rte_vect_cast_asm_impl.h"
+
+#else
+
 static __rte_always_inline __m256i
 __lasx_cast_128(__m128i src)
 {
@@ -173,6 +179,7 @@ __lasx_extract_128_hi(__m256i src)
 	return dest;
 }
 
+#endif /* !__clang__ */
 
 #endif /* __loongarch_asx_sx_conv */
 
